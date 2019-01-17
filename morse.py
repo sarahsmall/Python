d970d0040afa72df80e78e3bd72bc1a03fabef99
sarahsmall <37545978+sarahsmall@users.noreply.github.com>
2019-01-17 16:31:21 +0000

    Morse Code
    Converts strings into Morse code in python.

---
+++ morse.py
@@ -0,0 +1,50 @@
+import sys
+def main():
+
+	#get user 
+	thisdict =	{
+	  "a": ".-",
+	  "b": "-...",
+	  "c": "-.-.",
+	  "d": "-..",
+	  "e": ".",
+	  "f": "..-.",
+	  "g": "--.",
+	  "h": "....",
+	  "i": "..",
+	  "j": ".---",
+	  "k": "-.-",
+	  "l": ".-..",
+	  "m": "--",
+	  "n": "-.",
+	  "o": "---",
+	  "p": ".--.",
+	  "q": "--.-",
+	  "r": ".-.",
+	  "s": "...",
+	  "t": "-",
+	  "u": "..-",
+	  "v": "...-",
+	  "w": ".--",
+	  "x": "-..-",
+	  "y": "-.--",
+	  "z": "--.."
+	 
+
+	}
+	words = raw_input("please enter a string: ")
+	words = words.split()
+
+	for x in words:
+
+		for i in x:
+			sys.stdout.write(thisdict[i])
+		sys.stdout.write(" ")
+
+
+
+	
+
+	#print (thisdict["a"]);
+
+main()
